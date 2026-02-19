c305c149ed28ee6bd3c10b8268bde638de5c989c
Misgana-design <misganawoldeselassie@gmail.com>
2026-02-19 10:47:52 +0300

    Admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Section } from "@/components/ui/Section";
 import { Button } from "@/components/ui/Button";
 import { LeadRequest } from "@/app/api/leads/route";
@@ -30,6 +30,45 @@ export default function AdminDashboard() {
     setIsLoading(false);
   };
 
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+    setPassword("");
+    setLeads([]);
+  };
+
+  const updateStatus = async (id: number | undefined, status: string) => {
+    try {
+      const res = await fetch("api/leads", {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ id, status }),
+      });
+
+      if (res.ok) {
+        setLeads((prev) =>
+          prev?.map((lead) => (lead.id === id ? { ...lead, status } : lead)),
+        );
+      }
+
+      if (!res.ok) throw new Error();
+    } catch (error) {
+      alert("Updating status failed");
+    }
+  };
+
+  const deleteLeads = async (id: number | undefined) => {
+    try {
+      const res = await fetch(`api/leads?id=${id}`, { method: "DELETE" });
+      if (!confirm("Are you sure you want to delete")) return;
+      if (res.ok) {
+        setLeads(leads?.filter((lead) => lead.id !== id));
+      }
+      if (!res.ok) throw new Error();
+    } catch (error) {
+      alert("Delete failed");
+    }
+  };
+
   if (!isAuthenticated) {
     return (
       <div className="animate-fade-in">
@@ -41,12 +80,13 @@ export default function AdminDashboard() {
             <h1 className="text-2xl font-bold mb-6">Admin Access</h1>
             <input
               type="password"
+              required
               placeholder="Enter Admin Password"
               className="w-full p-3 border rounded mb-4 outline-none focus:ring-2 focus:ring-blue-600"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
-            <Button className="w-full">Login</Button>
+            <Button className="w-full hover:cursor-pointer">Login</Button>
           </form>
         </Section>
       </div>
@@ -56,10 +96,17 @@ export default function AdminDashboard() {
   return (
     <div className="animate-fade-in">
       <Section>
+        {isLoading && "Loading"}
         <div className="flex justify-between items-center mb-10">
           <h1 className="text-3xl font-bold">Inbound Leads</h1>
           <Button variant="secondary" onClick={fetchLeads}>
             Refresh Data
+          </Button>
+          <Button
+            onClick={handleLogout}
+            className="bg-red-100 text-red-600 border border-red-100 hover:bg-red-100 hover:text-red-700 hover:cursor-pointer p-3"
+          >
+            Logout
           </Button>
         </div>
 
@@ -72,6 +119,14 @@ export default function AdminDashboard() {
                 <th className="p-4 font-semibold">Email</th>
                 <th className="p-4 font-semibold">Message</th>
                 <th className="p-4 font-semibold">Status</th>
+                <th className="p-4 font-semibold">Buttons</th>
+
+                {leads?.map(
+                  (lead) =>
+                    lead.status === "New" && (
+                      <th className="p-4 font-semibold"></th>
+                    ),
+                )}
               </tr>
             </thead>
             <tbody>
@@ -84,10 +139,12 @@ export default function AdminDashboard() {
               ) : (
                 leads?.map((lead) => (
                   <tr
-                    key={lead.id}
+                    key={lead?.id}
                     className="border-b hover:bg-slate-50 transition-colors"
                   >
-                    <td className="p-4 text-sm">{lead.date}</td>
+                    <td className="p-4 text-sm">
+                      {new Date(lead.created_at).toLocaleDateString()}
+                    </td>
                     <td className="p-4 font-medium">{lead.name}</td>
                     <td className="p-4 text-sm">{lead.email}</td>
                     <td className="p-4 text-sm text-slate-600 truncate max-w-xs">
@@ -98,6 +155,24 @@ export default function AdminDashboard() {
                         {lead.status}
                       </span>
                     </td>
+                    <td>
+                      <Button
+                        onClick={() => deleteLeads(lead.id)}
+                        variant="secondary"
+                      >
+                        Delete
+                      </Button>
+                    </td>
+                    {lead.status === "New" && (
+                      <td>
+                        <button
+                          onClick={() => updateStatus(lead.id, "Contacted")}
+                          className="hover:cursor-pointer"
+                        >
+                          Mark as contacted
+                        </button>
+                      </td>
+                    )}
                   </tr>
                 ))
               )}
