60e9c9453d0ea5d8357b43ffdcf35360daa6a813
Misgana-design <misganawoldeselassie@gmail.com>
2026-02-24 16:15:53 +0300

    navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import { Menu, X } from "lucide-react"; // Install via: npm install lucide-react
+import { Menu, X } from "lucide-react";  
 import { Button } from "@/components/ui/Button";
 
 const Navbar = () => {
